8b946a910acb7a8657a718060f560270c8fc3fcb
DESKTOP-0O1DLNK\pauli <paulius.cernius99@gmail.com>
2023-12-10 04:32:54 +0200

    Some forgotten changes to push

--- frontend/src/Pages/Catalog.js
+++ frontend/src/Pages/Catalog.js
@@ -1,31 +1,35 @@
-import { Grid } from "@mui/material";
+import { Button, Grid, Stack } from "@mui/material";
 import FilterSideBar from "../Components/FilterSideBar"
 import ItemFull from "../Objects/itemFull";
 import ItemMinimalView from "../Components/ItemMinimalView";
 import { useEffect } from "react";
 import {items, images} from "../Objects/Data";
-
+import { Link } from "react-router-dom";
 
 const Catalog = () => {
     useEffect(() => {
         console.log(items[0]);
     }, [])
 
-    return   <> 
-    <FilterSideBar />
-    <Grid container spacing={2}>
-        {
-            items.map((element, i) => (
-                <Grid item xs={3} key={i}>
-                    <ItemMinimalView 
-                        item={element}
-                        image={images[i]}
-                    />
-                </Grid>
-            ))
-        }
+    return   <Grid container spacing={2} sx={{padding: 3}}>
+    <Grid item xs={3}>
+        <FilterSideBar />
     </Grid>
-    </>
+    <Grid item xs={9}>
+        <Grid container spacing={2} style={{paddingTop: 100}}>
+            {
+                items.map((element, i) => (
+                    <Grid item xs={4} key={i}>
+                        <ItemMinimalView 
+                            item={element}
+                            image={images[i]}
+                        />
+                    </Grid>
+                ))
+            }
+        </Grid>
+    </Grid>
+    </Grid>
 }
 
 export default Catalog;
