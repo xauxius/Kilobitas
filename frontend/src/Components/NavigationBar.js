f483e81069dd816580b047788e7db21cc0e37960
DESKTOP-0O1DLNK\pauli <paulius.cernius99@gmail.com>
2024-01-05 01:13:31 +0200

    Navigation bar and home page

--- frontend/src/Components/NavigationBar.js
+++ frontend/src/Components/NavigationBar.js
@@ -1,48 +1,24 @@
 import { Link } from 'react-router-dom';
+import '../Styles/NavigationBar.css'; // Ensure you have the updated CSS imported
 
 const NavigationBar = () => {
-    return <div className="nav-content">
-        <div className="nav-button">
-            <Link to="/">
-                Namai
-            </Link>
+    return (
+        <div className="nav-content">
+            <div className="nav-left">
+                <div className="nav-button"><Link to="/">Namai</Link></div>
+                <div className="nav-button"><Link to="/Katalogas">Katalogas</Link></div>
+                <div className="nav-button"><Link to="/Krepšelis">Krepšelis</Link></div>
+                <div className="nav-button"><Link to="/Administracinis">Administracinis puslapis</Link></div>
+                <div className="nav-button"><Link to="/Prekių-administravimas">Prekių administravimas</Link></div>
+            </div>
+            <div className="nav-right">
+                <div className="nav-button"><Link to="/Profilis">Profilis</Link></div>
+                <div className="nav-divider"></div> {/* Add a vertical divider */}
+                <div className="nav-button"><Link to="/Registruotis">Registruotis</Link></div>
+                <div className="nav-button"><Link to="/Prisijungti">Prisijungti</Link></div>
+            </div>
         </div>
-        <div className="nav-button">
-            <Link to="/Katalogas">
-                Katalogas
-            </Link>
-        </div>
-        <div className="nav-button">
-            <Link to="/Krepšelis">
-                Krepšelis
-            </Link>
-        </div>
-        <div className="nav-button">
-            <Link to="/Profilis">
-                Profilis
-            </Link>
-        </div>
-        <div className="nav-button">
-            <Link to="/Administracinis">
-                Administracinis puslapis
-            </Link>
-        </div>
-        <div className="nav-button">
-            <Link to="/Prekių-administravimas">
-                Prekių administravimas
-            </Link>
-        </div>
-        <div className="nav-button">
-            <Link to="/Registruotis">
-                Registruotis
-            </Link>
-        </div>
-        <div className="nav-button">
-            <Link to="/Prisijungti">
-                Prisijungti
-            </Link>
-        </div>
-    </div>
+    );
 }
 
 export default NavigationBar;
