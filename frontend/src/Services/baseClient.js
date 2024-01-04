870cfb5b2823f7fb2c41ee3ffe8b96dbacbfc872
Shiuw <r.mantas2002@gmail.com>
2024-01-04 10:47:51 +0200

    oips
    ops

--- frontend/src/Services/baseClient.js
+++ frontend/src/Services/baseClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseClient = axios.create({
-  baseURL: "http://localhost:7259",
+  baseURL: "http://localhost:5216",
   headers: {
     "Content-Type": "application/json",
   },
